0d956056a1cc08e555642ea29d36642b0c6f6d8e
ElDi046 <47604203+ElDi046@users.noreply.github.com>
2022-03-24 18:31:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/todolist/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.todolist.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.startButton.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_captureFragment)
+        }
+    }
+}
